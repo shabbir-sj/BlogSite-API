edd8c4e74ebb7d801845493f429d42c3392a2076
Shabbir Hussain Bohra <shabbir.bohra@mindfiresolutions.com>
2017-07-24 13:05:03 +0530

    blogsite: set static url.

--- settings/settings.py
+++ settings/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_ROOT = BASE_DIR + '/static/'
-STATIC_URL = '/'
+STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = True
 
